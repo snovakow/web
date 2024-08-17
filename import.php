d8c4e37568a95c8a654dab0c3baa773210d5eefc
ScottNovakowski <snovakow@gmail.com>
2024-08-17 19:29:25 -0400

    Update import.php

--- import.php
+++ import.php
@@ -1,349 +1,72 @@
 <?php
-	$version=1;
-	
-	if(!isset($_POST['version'])) die();
-	if($_POST['version']!=$version) die();
+$version = 1;
 
-	$isWeb=true;
-	$platform="Web";
-	$platformID=3;
-	$videoTitle = NULL;
+if (!isset($_GET['version'])) die();
+if ($_GET['version'] != $version) die();
 
-	$vrArray=true;
-	if(isset($_GET['platform'])) {
-		die("Disabled");
-		
-		$platform=$_GET['platform'];
-		if($platform!="Android" && $platform!="iOS") die();
-		$isWeb=false;
-		if($platform=="iOS") $platformID=1;
-		else $platformID=2;
-	} else {
-		if(!isset($_POST['platform'])) die();
-		$platform=$_POST['platform'];
-		if($platform=="Web") {
-			$platformID=3;
-			$vrArray=false;
-		} else if($platform=="Android") {
-			$platformID=2;
-		} else if($platform=="iOS") {
-			$platformID=1;
-		} else {
-			die();			
-		}
+if (!isset($_GET['puzzleClues'])) die();
+if (!isset($_GET['puzzleFilled'])) die();
+if (!isset($_GET['clueCount'])) die();
+if (!isset($_GET['simple'])) die();
+if (!isset($_GET['naked2'])) die();
+if (!isset($_GET['naked3'])) die();
+if (!isset($_GET['naked4'])) die();
+if (!isset($_GET['hidden2'])) die();
+if (!isset($_GET['hidden3'])) die();
+if (!isset($_GET['hidden4'])) die();
+if (!isset($_GET['yWing'])) die();
+if (!isset($_GET['xyzWing'])) die();
+if (!isset($_GET['xWing'])) die();
+if (!isset($_GET['swordfish'])) die();
+if (!isset($_GET['jellyfish'])) die();
+if (!isset($_GET['uniqueRectangle'])) die();
+if (!isset($_GET['phistomefel'])) die();
+if (!isset($_GET['superpositions'])) die();
+if (!isset($_GET['bruteForce'])) die();
 
-		if(!isset($_POST['client'])) die();
-		$client=$_POST['client'];
-		if($client=="ARTE") $clientID=2;
-		else if($client=="LiquidCinema") $clientID=1;
-		else die();
-		
-		if(!isset($_POST['device'])) die();
-		if(!isset($_POST['app_version'])) die();
-		if(!isset($_POST['os_version'])) die();
-		if(!isset($_POST['time'])) die();
-		if(!isset($_POST['hrot'])) die();
-		if(!isset($_POST['vrot'])) die();
-		if(!isset($_POST['count'])) die();
-		if($vrArray) {
-			if(!isset($_POST['vr'])) die();
-		}
+$puzzleClues = $_GET['puzzleClues'];
+$puzzleFilled = $_GET['puzzleFilled'];
+$clueCount = $_GET['clueCount'];
+$simple = $_GET['simple'];
+$naked2 = $_GET['naked2'];
+$naked3 = $_GET['naked3'];
+$naked4 = $_GET['naked4'];
+$hidden2 = $_GET['hidden2'];
+$hidden3 = $_GET['hidden3'];
+$hidden4 = $_GET['hidden4'];
+$yWing = $_GET['yWing'];
+$xyzWing = $_GET['xyzWing'];
+$xWing = $_GET['xWing'];
+$swordfish = $_GET['swordfish'];
+$jellyfish = $_GET['jellyfish'];
+$uniqueRectangle = $_GET['uniqueRectangle'];
+$phistomefel = $_GET['phistomefel'];
+$superpositions = $_GET['superpositions'];
+$bruteForce = $_GET['bruteForce'];
 
-		if(!isset($_POST['video_path'])) die();
-		if(!isset($_POST['video_file'])) die();
-		if(isset($_POST['video_title'])) $videoTitle=$_POST['video_title'];
-	}
-	
-	include 'db.php';
-	
-	$width=64.0;
-	$height=32.0;
-	$timeRate=4.0;
+$servername = "localhost";
+$username = "snovakow";
+$password = "kewbac-recge1-Fiwpux";
+$dbname = "sudoku";
 
-	$database = new LWMySQL();
+try {
+	$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+	// set the PDO error mode to exception
+	$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	echo "Connected successfully";
 
-	$test = ($clientID!=2);
-	//$test = true;
-	
-	if($videoTitle===NULL) {
-		$sqlVideo = "INSERT INTO `video`(`Path`, `File`, `Created`, `Updated`) 
-					VALUES (:VideoPath, :VideoFile, FROM_UNIXTIME(:Date), FROM_UNIXTIME(:Date)) 
-					ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id), `Updated` = FROM_UNIXTIME(:Date), `Count` = `Count` + 1";
-		if($test)
-		$sqlVideo = "INSERT INTO `video2`(`Path`, `File`, `Created`, `Updated`) 
-					VALUES (:VideoPath, :VideoFile, FROM_UNIXTIME(:Date), FROM_UNIXTIME(:Date)) 
-					ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id), `Updated` = FROM_UNIXTIME(:Date), `Count` = `Count` + 1";
-		
-		$stmtVideo = $database->db->prepare($sqlVideo);
-		$stmtVideo->bindParam(':VideoPath', $videoPath, PDO::PARAM_STR);
-		$stmtVideo->bindParam(':VideoFile', $videoFile, PDO::PARAM_STR);
-		$stmtVideo->bindParam(':Date', $date, PDO::PARAM_STR);
-	} else {
-		$sqlVideo = "INSERT INTO `video`(`Path`, `File`, `Title`, `Created`, `Updated`) 
-					VALUES (:VideoPath, :VideoFile, :VideoTitle, FROM_UNIXTIME(:Date), FROM_UNIXTIME(:Date)) 
-					ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id), `Title` = :VideoTitle, `Updated` = FROM_UNIXTIME(:Date), `Count` = `Count` + 1";
-		if($test)
-		$sqlVideo = "INSERT INTO `video2`(`Path`, `File`, `Title`, `Created`, `Updated`) 
-					VALUES (:VideoPath, :VideoFile, :VideoTitle, FROM_UNIXTIME(:Date), FROM_UNIXTIME(:Date)) 
-					ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id), `Title` = :VideoTitle, `Updated` = FROM_UNIXTIME(:Date), `Count` = `Count` + 1";
-		
-		$stmtVideo = $database->db->prepare($sqlVideo);
-		$stmtVideo->bindParam(':VideoPath', $videoPath, PDO::PARAM_STR);
-		$stmtVideo->bindParam(':VideoFile', $videoFile, PDO::PARAM_STR);
-		$stmtVideo->bindParam(':VideoTitle', $videoTitle, PDO::PARAM_STR);
-		$stmtVideo->bindParam(':Date', $date, PDO::PARAM_STR);
-	}
-  
-	$sql = 	"INSERT INTO `entry`(`videoID`, `clientID`, `platformID`, `Device`, `SoftwareVersion`, `AppVersion`, `mediumID`, `TimeInterval`, `Date`) 
-			VALUES (:videoID, :clientID, :platformID, :Device, :SoftwareVersion, :AppVersion, :mediumID, :TimeInterval, FROM_UNIXTIME(:Date))";
-	if($isWeb) {
-		$sql = 	"INSERT INTO `entry`(`videoID`, `clientID`, `platformID`, `Device`, `SoftwareVersion`, `AppVersion`, `mediumID`, `TimeInterval`) 
-				VALUES (:videoID, :clientID, :platformID, :Device, :SoftwareVersion, :AppVersion, :mediumID, :TimeInterval)";
-		if($test)
-		$sql = 	"INSERT INTO `entry2`(`videoID`, `clientID`, `platformID`, `Device`, `SoftwareVersion`, `AppVersion`, `mediumID`, `TimeInterval`) 
-				VALUES (:videoID, :clientID, :platformID, :Device, :SoftwareVersion, :AppVersion, :mediumID, :TimeInterval)";
-	}	
+	$sql = "INSERT INTO puzzles (puzzleClues, puzzleFilled, clueCount, simple, naked2, naked3, naked4, hidden2, hidden3, hidden4, 
+			yWing, xyzWing, xWing, swordfish, jellyfish, uniqueRectangle, phistomefel, superpositions, bruteForce) 
+			VALUES ($puzzleClues, $puzzleFilled, $clueCount, $simple, $naked2, $naked3, $naked4, $hidden2, $hidden3, $hidden4, 
+			$yWing, $xyzWing, $xWing, $swordfish, $jellyfish, $uniqueRectangle, $phistomefel, $superpositions, $bruteForce)";
+	// use exec() because no results are returned
+	$conn->exec($sql);
+	echo "New record created successfully";
+} catch (PDOException $e) {
+	echo "Connection failed: " . $e->getMessage();
+}
 
-	$stmt = $database->db->prepare($sql);
-	$stmt->bindParam(':videoID', $id, PDO::PARAM_STR);
-	$stmt->bindParam(':clientID', $clientID, PDO::PARAM_INT);
-	$stmt->bindParam(':platformID', $platformID, PDO::PARAM_INT);
-	$stmt->bindParam(':Device', $device, PDO::PARAM_STR);
-	$stmt->bindParam(':SoftwareVersion', $softwareVersion, PDO::PARAM_STR);
-	$stmt->bindParam(':AppVersion', $appVersion, PDO::PARAM_STR);
-	$stmt->bindParam(':mediumID', $mediumID, PDO::PARAM_INT);
-	$stmt->bindParam(':TimeInterval', $timeInterval, PDO::PARAM_STR);
-	if(!$isWeb) {
-		$stmt->bindParam(':Date', $date, PDO::PARAM_STR);
-	}
-	
-	$glEntry=false;
-	if(isset($_POST['opengl_version'])&&isset($_POST['opengl_renderer'])&&isset($_POST['max_texture_size'])) {
-		$glEntry=true;
-		$sqlGL =	"INSERT INTO `entrygl`(`entryID`, `MaxTex`, `Version`, `Renderer`) 
-					VALUES (:entryID, :MaxTex, :Version, :Renderer)";
-		if($test)
-		$sqlGL =	"INSERT INTO `entrygl2`(`entryID`, `MaxTex`, `Version`, `Renderer`) 
-					VALUES (:entryID, :MaxTex, :Version, :Renderer)";
-		$stmtGL = $database->db->prepare($sqlGL);
-		$stmtGL->bindParam(':entryID', $entryID, PDO::PARAM_INT);
-		$stmtGL->bindParam(':MaxTex', $_POST['max_texture_size'], PDO::PARAM_INT);
-		$stmtGL->bindParam(':Version', $_POST['opengl_version'], PDO::PARAM_STR);
-		$stmtGL->bindParam(':Renderer', $_POST['opengl_renderer'], PDO::PARAM_STR);
-	}
-	
-	$sqlData =	"INSERT INTO `data`(`videoID`, `Time`, `HRot`, `VRot`, `platformID`, `VRMode`) 
-				VALUES (:videoID, :time, :hRot, :vRot, :platformID, :vr) ON DUPLICATE KEY UPDATE `Count` = `Count` + 1";
-	if($test)
-	$sqlData =	"INSERT INTO `data2`(`videoID`, `Time`, `HRot`, `VRot`, `platformID`, `VRMode`) 
-				VALUES (:videoID, :time, :hRot, :vRot, :platformID, :vr) ON DUPLICATE KEY UPDATE `Count` = `Count` + 1";
+$conn = null;
 
-	$stmtData = $database->db->prepare($sqlData);
-	$stmtData->bindParam(':videoID', $id, PDO::PARAM_INT);
-	$stmtData->bindParam(':time', $time, PDO::PARAM_INT);
-	$stmtData->bindParam(':hRot', $hRot, PDO::PARAM_INT);
-	$stmtData->bindParam(':vRot', $vRot, PDO::PARAM_INT);
-	$stmtData->bindParam(':platformID', $platformID, PDO::PARAM_INT);
-	$stmtData->bindParam(':vr', $vr, PDO::PARAM_INT);
-
-	$timeInterval=0.25;
-	if($isWeb) {
-		$times=explode(",", $_POST['time']);
-		$length=count($times);	
-		if($length==0) die();
-		
-		$hRots=explode(",", $_POST['hrot']);
-		if($length!=count($hRots)) die();	
-		
-		$vRots=explode(",", $_POST['vrot']);
-		if($length!=count($vRots)) die();	
-
-		$counts=explode(",", $_POST['count']);
-		if($length!=count($counts)) die();	
-
-		if($vrArray) {
-			$vrs=explode(",", $_POST['vr']);
-			if($length!=count($vrs)) die();	
-		}
-		
-		header("Access-Control-Allow-Origin: *");
-
-		$device=$_POST['device'];
-		$softwareVersion=$_POST['os_version'];
-		$appVersion=$_POST['app_version'];
-		$vr=0;
-		$mediumID=2;
-
-		$date=time();
-		$videoPath=$_POST['video_path'];
-		$videoFile=$_POST['video_file'];
-		$stmtVideo->execute();
-		$id = $database->db->lastInsertId();
-
-		$stmt->execute();
-
-		if($glEntry) {
-			$entryID = $database->db->lastInsertId();
-			$stmtGL->execute();
-		}
-
-		for($i=0; $i<$length; $i++) {
-			$time=$times[$i];
-			$hRot=$hRots[$i];
-			$vRot=$vRots[$i];
-			if($vrArray) {
-				$vr=$vrs[$i];
-			}
-			$stmtData->execute();			
-		}
-	} else {
-		die();
-		set_time_limit(60*60*24);
-		echo("Start<br>");
-		
-		//if($platform=="iOS") $files=glob('./data/2016_02/ios/*');
-		//else $files=glob('./data/2016_02/android/*');
-		if($platform=="iOS") $files=glob('../../heatmap/data/2016_03_11/ios/*');
-		else $files=glob('../../heatmap/data/2016_03_11/android/*');		
-		
-		$processCount=0;
-		
-		foreach($files as $file) {
-			$base=basename($file);
-
-			$date=filemtime($file);
-
-			$pos=strpos($base, "_2016");
-			if($pos===false) $pos=strpos($base, "_2015");
-			if($pos===false) $pos=strpos($base, "_%3F%3F%3F%3F");
-			if($pos===false) $pos=strpos($base, "_????");
-			if($pos===false) $pos=strpos($base, "_2000");
-			if($pos===false) $pos=strpos($base, "_2014");
-			if($pos===false) $pos=strpos($base, "_2559");
-			if($pos===false) {
-				echo 'date: '.$base.'<br>';
-				continue;
-			}
-			$name=substr($base, 0, $pos);
-			$name = str_replace("_", " ", $name);
-
-			//if($name != "Stratos") continue;
-			if($name != "Seppia") continue;
-
-			$client="ARTE";
-			$clientID=2;
-			$pos=strpos($base, "_".$client."_");
-			if($pos===false) { $client="LiquidCinema"; $clientID=1; $pos=strpos($base, "_".$client."_"); }
-			if($pos===false) {
-				echo 'client: '.$base.'<br>';
-				continue;
-			}
-			
-			$end=substr($base, $pos+2+strlen($client), -7);
-			$pos=strrpos($end, "_");
-			if($pos===false) {
-				echo 'end: '.$base.'<br>';
-				continue;
-			}
-			
-			$medium = substr($end, $pos+1);
-			if($medium=="local" || $medium=="stream") {
-				$end=substr($end, 0, $pos);
-				$pos=strrpos($end, "_");
-			} else {
-				$medium="";			
-				continue;
-			}
-			if($medium=="local") $mediumID=1;
-			if($medium=="stream") $mediumID=2;
-			
-			if($pos===false) {
-				echo 'appVersion: '.$base.'<br>';
-				continue;
-			}
-			$appVersion = substr($end, $pos+1);
-			
-			$end=substr($end, 0, $pos);
-
-			if(strpos($end, "Apple")!==false) {
-				$matches = array();
-				if (preg_match('#(\d+.\d+)$#', $end, $matches)) {
-					$softwareVersion = $matches[1];
-					$end=substr($end, 0, -strlen($softwareVersion));
-				} else {
-					echo 'OS version: '.$base.'<br>';
-					continue;
-				}
-			} else {
-				$pos=strrpos($end, "_");
-				$softwareVersion = substr($end, $pos+1);
-				$end=substr($end, 0, $pos);			
-			}
-			
-			$device = str_replace("_", " ", $end);
-			
-			$zh = gzopen($file, 'r') or die("can't open: $file");
-			$first=true;
-			$lines=array();
-			while ($line = gzgets($zh,1024)) {
-				if($first) {
-					$first=false;
-				} else {
-					$components=explode(",", $line);
-					if(count($components)==6) {
-						$lines[]=$components;
-					} else {
-						echo "ERROR: ".$base.": ".$line.'<br>';
-						$first=true;
-						break;
-					}
-				}
-				$line=null;
-				unset($line);
-			}
-			gzclose($zh) or die("can't close: $file");
-			
-			if($first) continue;
-
-			$len=count($lines);
-			if($len==0) continue;
-
-			$stmtVideo->execute();
-			$id = $database->db->lastInsertId();
-			
-			$stmt->execute();
-			
-			for ($i = 0; $i < $len; $i++) {
-				$components=$lines[$i];
-				
-				$frame=floatval($components[0]);
-				$hRot=floatval($components[2]);
-				$vRot=floatval($components[3]);
-
-				if($frame<0) { continue; }
-				if($hRot<-180.0) { echo("hRot ".$hRot.": ".$base."<br>"); continue; }
-				if($hRot>180.0) { echo("hRot ".$hRot.": ".$base."<br>"); continue; }
-				if($vRot<-180.0) { echo("vRot ".$vRot.": ".$base."<br>"); continue; }
-				if($vRot>180.0) { echo("vRot ".$vRot.": ".$base."<br>"); continue; }
-				if($vRot<-90.0) { $vRot=-180.0-$vRot; if($hRot<0.0) $hRot+=180.0; else $hRot-=180.0; }
-				if($vRot>90.0) { $vRot=180.0-$vRot; if($hRot<0.0) $hRot+=180.0; else $hRot-=180.0; }
-				$time=$frame/30.0;
-				$time=floor($time*$timeRate);
-
-				$hRot=floor((180.0-$hRot)*($width/360.0));
-				if($hRot==$width) $hRot-=$width;
-				$hRot=min($hRot, $width-1);
-				$hRot=max($hRot, 0);
-				$vRot=floor((90.0-$vRot)*($height/180.0));
-				$vRot=min($vRot, $height-1);
-				$vRot=max($vRot, 0);
-
-				$vr=$components[1];
-				$stmtData->execute($dataValues);
-			}
-			
-			$processCount++;
-		}
-		echo("Done ".$processCount." processed.<br>");
-	}	
-?>
+$processCount = 1;
+echo ("Done " . $processCount . " processed.<br>");
