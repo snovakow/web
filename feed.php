e03a33e0d28707d3f0946bcb066857f926bf357f
ScottNovakowski <snovakow@gmail.com>
2024-08-20 10:31:20 -0400

    Update feed.php

--- feed.php
+++ feed.php
@@ -14,6 +14,18 @@
 	</script>
 
 	<?php
+	function percentage($count, $total)
+	{
+		$precision = 100000;
+		$number = $count / $total;
+		$formatted = ceil(100 * $number * $precision) / $precision;
+		return rtrim(rtrim(sprintf('%f', $formatted), '0'), ".") . "%";
+	}
+	function printStat($title, $count, $total)
+	{
+		echo $title . ": " . percentage($count, $total) . " " . number_format($count) . "<br/>";
+	}
+
 	// if(!isset($_GET['id'])) die();
 
 	// header("Access-Control-Allow-Origin: *");
@@ -25,35 +37,92 @@
 
 	try {
 		$conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-		$stmt = $conn->prepare("SELECT `clueCount`, count(*) as count FROM puzzles GROUP BY `clueCount`");
-		$stmt->execute();
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::ATTR_STRINGIFY_FETCHES);
 
+		$startTime = microtime(true);
 		$total = 0;
 		$counts = array();
 
+		$stmt = $conn->prepare("SELECT `clueCount`, COUNT(*) as count FROM puzzles GROUP BY `clueCount`");
+		$stmt->execute();
 		$result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
-		foreach ($result as $clueCount => $row) {
-			$total += $row['count'];
-		}
-
-		$precision = 100000;
-
-		foreach ($result as $clueCount => $row) {
+		foreach ($result as $clueCount => $row) $total += $row['count'];
+		foreach ($result as $key => $row) {
 			$clueCount = $row['clueCount'];
 			$count = $row['count'];
 
-			$number = $count / $total;
-			$formatted = ceil(100 * $number * $precision) / $precision;
-			echo  $clueCount;
-			echo  ": ";
-			echo rtrim(rtrim(sprintf('%f', $formatted), '0'), ".");
-			echo  "% ";
-			echo  number_format($count);
-			echo  "<br/>";
+			printStat($clueCount . " Clues", $count, $total);
 		}
+		echo  "<br/>";
 
-		echo  "Total: " . number_format($total) . "<br/>";
+		$stmt = $conn->prepare("
+		SELECT
+		SUM(`has_naked2`) AS naked2,
+		SUM(`has_naked3`) AS naked3,
+		SUM(`has_naked4`) AS naked4,
+		SUM(`has_hidden2`) AS hidden2,
+		SUM(`has_hidden3`) AS hidden3,
+		SUM(`has_hidden4`) AS hidden4,
+		SUM(`has_yWing`) AS yWing,
+		SUM(`has_xyzWing`) AS xyzWing,
+		SUM(`has_xWing`) AS xWing,
+		SUM(`has_swordfish`) AS swordfish,
+		SUM(`has_jellyfish`) AS jellyfish,
+		SUM(`has_uniqueRectangle`) AS uniqueRectangle,
+		SUM(`has_phistomefel`) AS phistomefel
+		FROM puzzles WHERE `bruteForce` = 0");
+		$stmt->execute();
+		$solveTypes = $stmt->fetch();
+
+		$naked2 = $solveTypes['naked2'];
+		$naked3 = $solveTypes['naked3'];
+		$naked4 = $solveTypes['naked4'];
+		$hidden2 = $solveTypes['hidden2'];
+		$hidden3 = $solveTypes['hidden3'];
+		$hidden4 = $solveTypes['hidden4'];
+		$yWing = $solveTypes['yWing'];
+		$xyzWing = $solveTypes['xyzWing'];
+		$xWing = $solveTypes['xWing'];
+		$swordfish = $solveTypes['swordfish'];
+		$jellyfish = $solveTypes['jellyfish'];
+		$uniqueRectangle = $solveTypes['uniqueRectangle'];
+		$phistomefel = $solveTypes['phistomefel'];
+
+		printStat("Naked 2", $naked2, $total);
+		printStat("Naked 3", $naked3, $total);
+		printStat("Naked 4", $naked4, $total);
+		printStat("Hidden 2", $hidden2, $total);
+		printStat("Hidden 3", $hidden3, $total);
+		printStat("Hidden 4", $hidden4, $total);
+		printStat("yWing", $yWing, $total);
+		printStat("xyzWing", $xyzWing, $total);
+		printStat("xWing", $xWing, $total);
+		printStat("swordfish", $swordfish, $total);
+		printStat("jellyfish", $jellyfish, $total);
+		printStat("uniqueRectangle", $uniqueRectangle, $total);
+		printStat("phistomefel", $phistomefel, $total);
+		echo  "<br/>";
+
+		$stmt = $conn->prepare("SELECT `solveType`, COUNT(*) as count FROM puzzles GROUP BY `solveType`");
+		$stmt->execute();
+		$result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+		foreach ($result as $key => $row) {
+			$solveType = $row['solveType'];
+			$count = $row['count'];
+
+			$type = "Simples";
+			if ($solveType === "1") $type =  "Markers";
+			else if ($solveType === "2") $type = "Brute Force";
+			printStat($type, $count, $total);
+		}
+		echo  "<br/>";
+
+		echo  "Total Puzzles: " . number_format($total) . "<br/>";
+		echo  "<br/>";
+
+		$endtime = microtime(true);
+		$timediff = $endtime - $startTime;
+		echo $timediff . " seconds<br/>";
 	} catch (PDOException $e) {
 		echo "Error: " . $e->getMessage();
 	}
