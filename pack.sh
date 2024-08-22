715ce8168ae71fbf1bf6e19c671b808e592d311e
ScottNovakowski <snovakow@gmail.com>
2024-08-22 12:26:25 -0400

    Update pack.sh

--- pack.sh
+++ pack.sh
@@ -2,7 +2,7 @@ player="echo '\n***\npack player\n***\n';npx webpack"
 presenter="echo '\n***\npack presenter\n***\n';npx webpack --config webpack.config.presenter.js"
 
 echo
-echo "Options: all | resume | sudoku | birthday| bounder | home"
+echo "Options: all | resume | sudoku | sudokudev | birthday| bounder | home"
 echo
 
 cd ".."
@@ -23,6 +23,15 @@ do
 		echo "******"
 
 		cd "sudoku"
+		npx webpack
+		cd ".."
+		echo
+	fi
+	if [ "$build" = "all" ] || [ "$build" = "sudokudev" ] ; then
+		echo "sudokudev"
+		echo "******"
+
+		cd "sudokudev"
 		npx webpack
 		cd ".."
 		echo
@@ -55,8 +64,3 @@ do
 	fi
 	shift 1;
 done
-
-# 	rm -rf "./dist"
-# 	mkdir "dist"
-# 	cp -r "../../../repo/jkxr-cms-api-v5/public/player" "./dist/player"
-# 	cp "./index.html" "./dist/index.html"
