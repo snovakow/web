8f1ca0239d342f548d91ba8596d7013369f8bdf0
ScottNovakowski <snovakow@gmail.com>
2024-08-30 17:49:30 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-const initialize = (main, gtagId = false) => {
-	const url = new URL(main);
-	const search = new URLSearchParams(url.search);
-	const version = [...search.keys()][0];
+const initialize = (args = {}) => {
+	args.main = args.main ?? false;
+	args.gtag = args.gtag ?? false;
+	args.css = args.css ?? false;
+
+	let version = "0";
+	if (args.main) {
+		const url = new URL(args.main);
+		const search = new URLSearchParams(url.search);
+		version = [...search.keys()][0];
+	}
 	console.log("App Version: " + version);
 
-	if (gtagId) {
+	const lead = document.head.children[0];
+	if (args.gtag) {
 		const ga = document.createElement('script');
 		ga.async;
-		ga.src = `https://www.googletagmanager.com/gtag/js?id=${gtagId}`;
+		ga.src = `https://www.googletagmanager.com/gtag/js?id=${args.gtag}`;
 
 		const gtag = document.createElement('script');
 		gtag.textContent = `
@@ -15,12 +23,22 @@ const initialize = (main, gtagId = false) => {
 			function gtag() { dataLayer.push(arguments); }
 			gtag('js', new Date());
 		
-			gtag('config', '${gtagId}');
+			gtag('config', '${args.gtag}');
 		`;
 
-		const lead = document.head.children[0];
 		document.head.insertBefore(ga, lead);
 		document.head.insertBefore(gtag, lead);
+	}
+
+	if (args.css) {
+		let array = args.css;
+		if (typeof array === "string") array = [array];
+		for (const css of array) {
+			const link = document.createElement('link');
+			link.rel = "stylesheet";
+			link.href = css + "?" + version;
+			document.head.insertBefore(link, lead);
+		}
 	}
 
 	return new Promise((resolve) => {
