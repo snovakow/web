402d896a19f3e99347151e276087b7484c7cebe4
snovakow <snovakow@gmail.com>
2025-02-09 14:49:13 -0500

    Update finder.js

--- sudoku/finder.js
+++ sudoku/finder.js
@@ -1,5 +1,6 @@
 import { CellCandidate, Grid } from "../sudokulib/Grid.js";
 import { sudokuGenerator, STRATEGY } from "../sudokulib/generator.js";
+import { simpleHidden } from "../sudokulib/solver.js";
 
 const cells = new Grid();
 for (let i = 0; i < 81; i++) cells[i] = new CellCandidate(i);
@@ -18,42 +19,39 @@ const emptyCount = (cells) => {
 }
 
 const fillSolve = () => {
-	let hiddenSimple = 0;
 	let remaining = emptyCount(cells);
 	while (remaining > 0) {
 		if (simpleHidden(cells)) {
-			hiddenSimple++;
 			remaining--;
 		}
 		else break;
 	}
 
-	if (remaining > 0) false;
-	return true
+	return (remaining === 0);
 }
 
 const step = () => {
 	const [clueCount, puzzleFilled] = sudokuGenerator(cells);
 
 	const data = {
-		puzzle: cells.string(),
+		puzzleClues: cells.string(),
 		cells: cells.toData()
 	};
 
 	for (const cell of cells) if (cell.symbol === 0) cell.fill();
 
 	const solved = fillSolve(cells, simples, []);
-	data.puzzleClues = data.puzzle;
 	data.puzzleFilled = puzzleFilled.join('');
 	data.clueCount = clueCount;
 
 	data.solved = solved;
 
 	puzzleCount++;
+	data.id = puzzleCount;
+	data.solved = solved;
 
 	postMessage(data);
 
-	if (puzzleStrings) return puzzleStrings.length > 0;
 	return solved;
 };
 
