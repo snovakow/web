1aca7c0e23fbcc3b6ce74f871ca43ed2968029ac
snovakow <snovakow@gmail.com>
2025-03-14 21:00:06 -0400

    Update process.js

--- sudokulib/process.js
+++ sudokulib/process.js
@@ -133,41 +133,257 @@ const puzzleHexGrid = (puzzleDataHex) => {
 	return [puzzle, grid];
 }
 
-const puzzleGridBase64 = (clues, filled) => {
-	const binaryFilled = [];
-	for (let row = 1; row < 8; row++) {
-		for (let i = 0; i < 8; i++) {
-			const index = row * 9 + i;
-			const symbol = parseInt(filled[index]) - 1;
+const base64chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+const base64index = new Map();
+for (let i = 0; i < base64chars.length; i++) base64index.set(base64chars[i], i);
 
-			let encode = symbol;
-			if (i < encode) encode--;
-
-			const symbolBit1 = encode % 2;
-			encode = Math.floor(encode / 2);
-			const symbolBit2 = encode % 2;
-			encode = Math.floor(encode / 2);
-			const symbolBit3 = encode % 2;
-
-			binaryFilled.push(symbolBit3);
-			binaryFilled.push(symbolBit2);
-			binaryFilled.push(symbolBit1);
+class Possibilty {
+	constructor() {
+		this.mask = 0x01FF;
+		this.remaining = 9;
+	}
+	bitCount() {
+		switch (this.remaining) {
+			case 9: return 4;
+			case 8: return 3;
+			case 7: return 3;
+			case 6: return 3;
+			case 5: return 3;
+			case 4: return 2;
+			case 3: return 2;
+			case 2: return 1;
+		}
+		return 0;
+	}
+	compress(symbol) {
+		const symbolIndex = symbol - 1;
+		let compressed = symbolIndex;
+		for (let i = 0; i < symbolIndex; i++) {
+			const bit = (this.mask >>> i) & 0x1;
+			if (bit === 0x0) compressed--;
+		}
+		return compressed;
+	}
+	decompress(compressed) {
+		let ones = -1;
+		for (let i = 0; i < 9; i++) {
+			const bit = (this.mask >>> i) & 0x1;
+			if (bit === 0x1) ones++;
+			if (ones === compressed) return i + 1;
+		}
+		return 0;
+	}
+	bitFill(symbol, binary) {
+		const bitIndex = this.bitCount() - 1;
+		if (bitIndex < 0) return;
+		const symbolIndex = this.compress(symbol);
+		for (let shift = bitIndex; shift >= 0; shift--) {
+			binary.push((symbolIndex >>> shift) & 0x1);
 		}
 	}
-	const gridLength = 56;
-	const bitLength = gridLength * 3;
-	const byteLength = bitLength / 8; // 21
-	const hexFilled = new Uint8Array(byteLength);
-	let index = 0;
-	for (let i = 0; i < byteLength; i++) {
-		let char = 0x00;
-		for (let offset = 0; offset < 8; offset++) {
-			if (binaryFilled[index] !== 0) char |= 0x80 >>> offset;
-			index++;
-		}
-		hexFilled[i] = char;
+	reduce(symbol) {
+		const symbolIndex = symbol - 1;
+
+		const mask = this.mask;
+		this.mask &= ~(0x1 << symbolIndex);
+		if (mask === this.mask) return;
+
+		this.remaining--;
 	}
-	return puzzleCluesHex(clues) + bin2hex(hexFilled);
+}
+const reducePossibility = (symbol, index, possibilities) => {
+	const row = Math.floor(index / 9);
+	const col = index % 9;
+	for (let i = row + 1; i < 9; i++) {
+		const group = possibilities[i * 9 + col];
+		group.reduce(symbol);
+	}
+	for (let i = col + 1; i < 9; i++) {
+		const group = possibilities[row * 9 + i];
+		group.reduce(symbol);
+	}
+	const boxRow = 3 * Math.floor(row / 3);
+	const boxCol = 3 * Math.floor(col / 3);
+	const boxi = (row - boxRow) * 3 + (col - boxCol);
+	for (let i = boxi + 1; i < 9; i++) {
+		const m = boxRow + Math.floor(i / 3);
+		const n = boxCol + i % 3;
+		const boxIndex = m * 9 + n;
+		if (boxIndex <= index) continue;
+		const group = possibilities[boxIndex];
+		group.reduce(symbol);
+	}
+}
+const compressGrid = (grid) => {
+	const possibilities = [];
+	for (let i = 0; i < 81; i++) {
+		possibilities.push(new Possibilty());
+	}
+
+	const binary = [];
+	for (let index = 0; index < 81; index++) {
+		const symbol = grid[index];
+		const possibility = possibilities[index];
+		possibility.bitFill(symbol, binary);
+
+		reducePossibility(symbol, index, possibilities);
+	}
+
+	return binary;
 }
 
-export { puzzleGridHex, puzzleHexGrid };
+const decompressGrid = (binary) => {
+	const possibilities = [];
+	for (let i = 0; i < 81; i++) {
+		possibilities.push(new Possibilty());
+	}
+
+	let bitIndex = 0;
+	const grid = new Uint8Array(81);
+	for (let index = 0; index < 81; index++) {
+		const possibility = possibilities[index];
+		const bitCount = possibility.bitCount();
+		let compressed = 0x0;
+		for (let shift = bitCount - 1; shift >= 0; shift--) {
+			if (bitIndex === binary.length) {
+				console.log("error");
+				break;
+			}
+			const bit = binary[bitIndex];
+			compressed |= bit << shift;
+			bitIndex++;
+		}
+		const symbol = possibility.decompress(compressed);
+		grid[index] = symbol;
+		reducePossibility(symbol, index, possibilities);
+	}
+	binary.splice(0, bitIndex);
+
+	return grid;
+}
+
+const binaryToBase64 = (binaryArray) => {
+	const base = 6;
+	const bitLength = binaryArray.length;
+	const baseLength = Math.ceil(bitLength / base);
+	let baseFilled = "";
+	let index = 0;
+	const shiftBit = 0x1 << (base - 1);
+	for (let i = 0; i < baseLength; i++) {
+		let char = 0x0;
+		for (let offset = 0; offset < base; offset++) {
+			if (index === binaryArray.length) break;
+			if (binaryArray[index] !== 0) char |= shiftBit >>> offset;
+			index++;
+		}
+		baseFilled += base64chars[char];
+	}
+	return baseFilled;
+}
+
+// 00 clue: 2 bit
+// 01 4 bit symbol: 6 bit
+// 10 empty mask: 2 bit
+// 11 9 bit mask: 11 bit
+const puzzleGridBase64 = (board) => {
+	const binaryData = compressGrid(board.puzzleSolved);
+
+	for (let i = 0; i < 81; i++) {
+		if (board.startCells[i].symbol > 0) {
+			binaryData.push(0, 0);
+			continue;
+		}
+
+		const cell = board.cells[i];
+		if (cell.symbol > 0) {
+			binaryData.push(0, 1);
+			const symbolBase = cell.symbol - 1;
+			for (let offset = 3; offset >= 0; offset--) {
+				binaryData.push((symbolBase >>> offset) & 0x1);
+			}
+			continue;
+		}
+
+		if (cell.mask === 0x0) {
+			binaryData.push(1, 0);
+			continue;
+		}
+
+		binaryData.push(1, 1);
+		const maskBase = cell.mask >>> 1;
+		for (let offset = 8; offset >= 0; offset--) {
+			binaryData.push((maskBase >>> offset) & 0x1);
+		}
+	}
+
+	return binaryToBase64(binaryData);
+}
+
+const throwError = "Board data decompress error!";
+const puzzleBase64Grid = (board, puzzleDataBase64) => {
+	// min = 81*2/6 = 27
+	if (!puzzleDataBase64 || puzzleDataBase64.length <= 27) throw new Error(throwError);
+
+	const bits = [];
+	for (let i = 0; i < puzzleDataBase64.length; i++) {
+		const char = puzzleDataBase64[i];
+		const base64 = base64index.get(char);
+
+		for (let offset = 5; offset >= 0; offset--) {
+			bits.push((base64 >>> offset) & 0x1);
+		}
+	}
+	const gridSeed = decompressGrid(bits);
+	console.log(bits.length, bits.length / 6);
+
+	for (let i = 0; i < 81; i++) board.puzzleSolved[i] = gridSeed[i];
+
+	let bitIndex = 0;
+	for (let i = 0; i < 81; i++) {
+		const requiredRemaining = (81 - i) * 2;
+		if (bitIndex > bits.length - requiredRemaining) throw new Error(throwError);
+
+		const bit1 = bits[bitIndex++];
+		const bit2 = bits[bitIndex++];
+		if (bit1 === 0 && bit2 === 0) {
+			board.startCells[i].symbol = board.puzzleSolved[i];
+			board.cells[i].symbol = board.puzzleSolved[i];
+			board.cells[i].mask = 0x0000;
+			continue;
+		}
+
+		board.startCells[i].symbol = 0;
+		if (bit1 === 0 && bit2 === 1) {
+			if (bitIndex > bits.length - 4) throw new Error(throwError);
+			let symbol = 0;
+			for (let i = 3; i >= 0; i--) {
+				const bit = bits[bitIndex++];
+				symbol |= bit << i;
+			}
+			symbol++;
+			board.cells[i].symbol = symbol;
+			board.cells[i].mask = 0x0000;
+			continue;
+		}
+
+		board.cells[i].symbol = 0;
+		if (bit1 === 1 && bit2 === 0) {
+			board.cells[i].mask = 0x0000;
+			continue;
+		}
+
+		// if (bit1===1 && bit2===1) 
+		if (bitIndex > bits.length - 9) throw new Error(throwError);
+
+		let mask = 0;
+		for (let i = 8; i >= 0; i--) {
+			const bit = bits[bitIndex++];
+			mask |= bit << i;
+		}
+		mask <<= 1;
+
+		board.cells[i].mask = mask;
+	}
+}
+
+export { puzzleGridHex, puzzleHexGrid, puzzleGridBase64, puzzleBase64Grid };
